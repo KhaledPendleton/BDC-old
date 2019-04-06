f9354b71b16e64aedf13ec5c50e6697c9b510f92
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-06 19:28:54 -0400

    Remove unnecessary dependency definitions

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -8,11 +8,9 @@ use BDC\Framework\Rendering\TemplateRenderer;
 
 // Factories
 use BDC\Framework\Rendering\TwigTemplateRendererFactory;
-use BDC\Newsletter\Presentation\SubscribeFormFactory;
 
 // Implementable Classes
 use BDC\Framework\Rendering\TemplateDirectory;
-use BDC\Newsletter\Presentation\SubscribeForm;
 
 $dependencies = array();
 
@@ -22,6 +20,5 @@ $dependencies[TemplateDirectory::class] = autowire()
 $dependencies[TemplateRenderer::class] = factory([TwigTemplateRendererFactory::class, 'create']);
 
 // Newsletter
-$dependencies[SubscribeFormFactory::class] = factory([SubscribeFormFactory::class], 'createFromRequest');
 
 return $dependencies;
