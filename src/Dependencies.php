1b08db3cbb8b8418d62de2829eb7631b9f8cf321
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-06 19:17:46 -0400

    Wired up subscribe form/factory dependencies

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -8,9 +8,11 @@ use BDC\Framework\Rendering\TemplateRenderer;
 
 // Factories
 use BDC\Framework\Rendering\TwigTemplateRendererFactory;
+use BDC\Newsletter\Presentation\SubscribeFormFactory;
 
 // Implementable Classes
 use BDC\Framework\Rendering\TemplateDirectory;
+use BDC\Newsletter\Presentation\SubscribeForm;
 
 $dependencies = array();
 
@@ -18,5 +20,8 @@ $dependencies = array();
 $dependencies[TemplateDirectory::class] = autowire()
     ->constructorParameter('rootDirectory', ROOT_DIR);
 $dependencies[TemplateRenderer::class] = factory([TwigTemplateRendererFactory::class, 'create']);
+
+// Newsletter
+$dependencies[SubscribeFormFactory::class] = factory([SubscribeFormFactory::class], 'createFromRequest');
 
 return $dependencies;
