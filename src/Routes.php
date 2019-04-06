987a6f2b891c208c2ea2795d2df2a38ae9216746
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-05 23:50:49 -0400

    Refactored Routes.php

--- src/Routes.php
+++ src/Routes.php
@@ -1,9 +1,10 @@
 <?php declare(strict_types = 1);
 
+use BDC\Framework\Routing\HttpMethod;
 use BDC\Framework\Routing\Route;
 
 $routes = array();
 
-$routes[] = new Route('GET', '/', 'BDC\FrontPage\Presentation\FrontPageController#show');
+$routes[] = new Route(HttpMethod::GET, '/', 'BDC\FrontPage\Presentation\FrontPageController#show');
 
 return $routes;
