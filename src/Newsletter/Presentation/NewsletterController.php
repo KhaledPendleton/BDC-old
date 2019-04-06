3072b6a7e55db81da236ef13705618bc2c92b53c
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-06 17:49:02 -0400

    Added functionality and styles to newsletter success page. Fixed bug in templates where links weren't referenced correctly

--- src/Newsletter/Presentation/NewsletterController.php
+++ src/Newsletter/Presentation/NewsletterController.php
@@ -20,15 +20,22 @@ final class NewsletterController
     public function show(): Response
     {}
 
-    public function success(): Response
+    public function success(Request $request, array $vars): Response
     {
-        $content = $this->templateRenderer->render('Pages/NewsletterSuccess.html');
+        $firstName = ucwords($request->query->get('first_name'));
+        $content = $this->templateRenderer->render(
+            'Pages/NewsletterSuccess.html',
+            array('firstName' => $firstName)
+        );
+
         $response = new Response($content, Response::HTTP_OK);
+        return $response;
     }
 
     public function subscribe(Request $request, array $vars): Response
     {
-        $response = new RedirectResponse('/newsletter/success');
+        $firstName = $request->get('first_name');
+        $response = new RedirectResponse('/newsletter/success?first_name='.$firstName);
         return $response;
     }
 }
