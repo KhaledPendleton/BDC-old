a0d597bf39ade73fef4fab0d63da8c1a921a896d
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-06 17:19:20 -0400

    Added routes for newsletter signup and success message

--- src/Routes.php
+++ src/Routes.php
@@ -6,5 +6,8 @@ use BDC\Framework\Routing\Route;
 $routes = array();
 
 $routes[] = new Route(HttpMethod::GET, '/', 'BDC\FrontPage\Presentation\FrontPageController#show');
+$routes[] = new Route(HttpMethod::GET, '/newsletter', 'BDC\Newsletter\Presentation\NewsletterController#show');
+$routes[] = new Route(HttpMethod::GET, '/newsletter/success', 'BDC\Newsletter\Presentation\NewsletterController#success');
+$routes[] = new Route(HttpMethod::POST, '/newsletter/signup', 'BDC\Newsletter\Presentation\NewsletterController#signup');
 
 return $routes;
