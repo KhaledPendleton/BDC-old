57eca548cb87a23909f17d7845efd6380a4451a5
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-05 23:35:24 -0400

    Refactored dependencies.php

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -3,12 +3,20 @@
 use function DI\autowire;
 use function DI\factory;
 
+// Interfaces
 use BDC\Framework\Rendering\TemplateRenderer;
-use BDC\Framework\Rendering\TemplateDirectory;
+
+// Factories
 use BDC\Framework\Rendering\TwigTemplateRendererFactory;
 
+// Implementable Classes
+use BDC\Framework\Rendering\TemplateDirectory;
+
 $dependencies = array();
-$dependencies[TemplateDirectory::class] = autowire()->constructorParameter('rootDirectory', ROOT_DIR);
+
+// Template Rendering
+$dependencies[TemplateDirectory::class] = autowire()
+    ->constructorParameter('rootDirectory', ROOT_DIR);
 $dependencies[TemplateRenderer::class] = factory([TwigTemplateRendererFactory::class, 'create']);
 
 return $dependencies;
