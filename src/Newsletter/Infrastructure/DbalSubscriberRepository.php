408ee58555a580558f614e7b687e02a2566da7ee
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-07 16:57:07 -0400

    Subscriber repository findById updated

--- src/Newsletter/Infrastructure/DbalSubscriberRepository.php
+++ src/Newsletter/Infrastructure/DbalSubscriberRepository.php
@@ -2,7 +2,35 @@
 
 namespace BDC\Newsletter\Infrastructure;
 
+use Doctrine\DBAL\Connection;
 use BDC\Newsletter\Domain\SubscriberRepository;
 
 final class DbalSubscriberRepository implements SubscriberRepository
-{}
+{
+    private $connection;
+
+    public function __construct(Connection $connection) {
+        $this->connection = $connection;
+    }
+
+    public function findByEmail(string $email)
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        $queryBuilder->addSelect('id');
+        $queryBuilder->addSelect('first_name');
+        $queryBuilder->addSelect('last_name');
+        $queryBuilder->addSelect('email');
+        $queryBuilder->from('subscriber');
+        $queryBuilder->where("email = {$queryBuilder->createNamedParameter($email)}");
+
+        $stmt = $queryBuilder->execute();
+        $row = $stmt->fetch();
+
+        if (!$row) {
+            return null;
+        }
+
+        return $row;
+    }
+}
