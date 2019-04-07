eb516da0784721fe007e3298cb3b774364d014db
Khaled Pendleton <khaledpen@hotmail.com>
2019-04-07 18:50:14 -0400

    Connected database via dependency injector container

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -2,7 +2,11 @@
 
 use function DI\autowire;
 use function DI\factory;
+use function DI\env;
 use function DI\get;
+
+// Third-party
+use Doctrine\DBAL\Connection;
 
 // Interfaces
 use BDC\Framework\Rendering\TemplateRenderer;
@@ -11,13 +15,36 @@ use BDC\Newsletter\Domain\SubscriberRepository;
 
 // Factories
 use BDC\Framework\Rendering\TwigTemplateRendererFactory;
+use BDC\Framework\Dbal\ConnectionFactory;
 
 // Implementable Classes
+use BDC\Framework\Dbal\ConnectionParamContainer;
 use BDC\Framework\Rendering\TemplateDirectory;
 use BDC\Newsletter\Infrastructure\DbalEmailSubscribedQuery;
 use BDC\Newsletter\Domain\DbalSubscriberRepository;
 
 $dependencies = array();
+
+// Database definitions
+$dependencies['db.name'] = env('DB_NAME', 'bdc');
+$dependencies['db.user'] = env('DB_USER', 'admin');
+$dependencies['db.password'] = env('DB_PASSWORD', 'password');
+$dependencies['db.host'] = env('DB_HOST', 'localhost');
+$dependencies['db.driver'] = env('DB_DRIVER', 'pdo_mysql');
+$dependencies['db.sslMode'] = env('DB_SSLMODE', '');
+$dependencies['db.port'] = env('DB_PORT', '');
+
+// Dbal Connection
+$dependencies[ConnectionParamContainer::class] = autowire()
+    ->constructorParameter('name', get('db.name'))
+    ->constructorParameter('user', get('db.user'))
+    ->constructorParameter('password', get('db.password'))
+    ->constructorParameter('host', get('db.host'))
+    ->constructorParameter('driver', get('db.driver'))
+    ->constructorParameter('sslMode', get('db.sslMode'))
+    ->constructorParameter('port', get('db.port'));
+
+$dependencies[Connection::class] = factory([ConnectionFactory::class, 'create']);
 
 // Template Rendering
 $dependencies[TemplateDirectory::class] = autowire()
--- src/Newsletter/Infrastructure/DbalEmailSubscribedQuery.php
+++ src/Newsletter/Infrastructure/DbalEmailSubscribedQuery.php
@@ -2,12 +2,27 @@
 
 namespace BDC\Newsletter\Infrastructure;
 
+use Doctrine\DBAL\Connection;
+
 use BDC\Newsletter\Application\EmailSubscribedQuery;
 
 final class DbalEmailSubscribedQuery implements EmailSubscribedQuery
 {
+    private $connection;
+
+    public function __construct(connection $connection) {
+        $this->connection = $connection;
+    }
+
     public function execute(string $email): bool
     {
-        return true;
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        $queryBuilder->select('count(*)');
+        $queryBuilder->from('subscriber');
+        $queryBuilder->where("email = {$queryBuilder->createNamedParameter($email)}");
+
+        $stmt = $queryBuilder->execute();
+        return (bool)$stmt->fetchColumn();
     }
 }
